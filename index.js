074d01ac03479e58bd866f19cf08b80db7e79643
yerinnnn <rinny999@naver.com>
2019-02-09 14:32:58 +0900

    add basic message card

---
+++ index.js
@@ -0,0 +1,11 @@
+
+//<![CDATA[
+  // // 사용할 앱의 JavaScript 키를 설정해 주세요.
+  Kakao.init('f76d220286c8862b47437d30637130ac');
+  // // 카카오링크 버튼을 생성합니다. 처음 한번만 호출하면 됩니다.
+  function sendLink() {
+    Kakao.Link.sendScrap({
+      requestUrl: 'https://developers.kakao.com'
+    });
+  }
+//]]>
